fe1996983a24080312155de6df7d5f97b754895a
Leo <44327236+leodasso@users.noreply.github.com>
2024-10-31 11:58:18 +0900

    add animation curve utils

--- Runtime/CurveUtils.cs
+++ Runtime/CurveUtils.cs
@@ -22,5 +22,75 @@ namespace Moonloop.Core {
 
             return highest - lowest;
         }
+
+        ///<summary> 
+        ///scales the animation curve so that it always goes from 0 to 1 time and 0 to 1 intensity 
+        ///</summary>
+        public static void NormalizeCurve(this AnimationCurve curve) 
+        {
+            float maxTime = 0;
+            float maxIntensity = 0;
+            for (int i = 0; i < curve.length; i++)
+            {
+                maxTime = Mathf.Max(maxTime, curve.keys[i].time);
+                maxIntensity = Mathf.Max(maxIntensity, curve.keys[i].value);
+            }
+
+            for (int i = 0; i < curve.length; i++)
+            {
+                Keyframe key = curve.keys[i];
+                key.time /= maxTime;
+                key.value /= maxIntensity;
+                curve.MoveKey(i, key);
+            }
+        }
+
+        /// <summary>
+        /// Scales the curve so that the time (x-axis) goes from 0 to 1
+        /// </summary>
+        public static void NormalizeCurveHorizontal( this AnimationCurve curve )
+        {
+            float maxTime = 0;
+            for (int i = 0; i < curve.length; i++)
+                maxTime = Mathf.Max(maxTime, curve.keys[i].time);
+
+            for (int i = 0; i < curve.length; i++)
+            {
+                Keyframe key = curve.keys[i];
+                key.time /= maxTime;
+                curve.MoveKey(i, key);
+            }
+        }
+
+        /// <summary>
+        /// Scales the curve so that the intensity (y-axis) goes from 0 to 1
+        public static void NormalizeCurveVertical( this AnimationCurve curve )
+        {
+            float maxIntensity = 0;
+            for (int i = 0; i < curve.length; i++)
+                maxIntensity = Mathf.Max(maxIntensity, curve.keys[i].value);
+
+            for (int i = 0; i < curve.length; i++)
+            {
+                Keyframe key = curve.keys[i];
+                key.value /= maxIntensity;
+                curve.MoveKey(i, key);
+            }
+        }
+        
+        /// <summary>
+        /// Sets all the points on the curve to have a constant tangent mode. This makes the curve look like stairs
+        /// </summary>
+        public static void MakeCurveConstant( this AnimationCurve curve )
+        {
+            #if UNITY_EDITOR
+            for (int i = 0; i < curve.length; i++)
+            {
+                Keyframe key = curve.keys[i];
+                UnityEditor.AnimationUtility.SetKeyLeftTangentMode(curve, i, UnityEditor.AnimationUtility.TangentMode.Constant);
+                UnityEditor.AnimationUtility.SetKeyRightTangentMode(curve, i, UnityEditor.AnimationUtility.TangentMode.Constant);
+            }
+            #endif
+        }
     }
 }
