53932ec1ff9175bb10af59165f5d1d0407445010
Leo <44327236+leodasso@users.noreply.github.com>
2025-02-14 20:45:34 +0900

    make coroutine helper that lets you return the coroutine

--- Runtime/CoroutineHelper.cs
+++ Runtime/CoroutineHelper.cs
@@ -6,23 +6,32 @@ namespace Moonloop.Core
 {
     public class CoroutineHelper : MonoBehaviour
     {
-        public static void NewCoroutine(IEnumerator coroutine, bool destroyOnLoad = false)
-        {
-            // get the name of the coroutine as a string
-            string coroutineName = coroutine.ToString();
-            GameObject coroutineHelper = new GameObject("Coroutine Helper: " + coroutineName);
+        static CoroutineHelper instance;
+        static CoroutineHelper persistentInstance;
 
-            if (!destroyOnLoad)
-                DontDestroyOnLoad(coroutineHelper);
-                
-            var coroutineHelperObject = coroutineHelper.AddComponent<CoroutineHelper>();
-            coroutineHelperObject.StartCoroutine(coroutineHelperObject.DoRoutineAndDestroy(coroutine));
+        public static Coroutine NewCoroutine(IEnumerator coroutine, bool dontDestroyOnLoad = false)
+        {
+            var helperInstance = EnsureInstance(dontDestroyOnLoad);
+            return helperInstance.StartCoroutine(coroutine);
         }
 
-        IEnumerator DoRoutineAndDestroy(IEnumerator coroutine)
+        static CoroutineHelper EnsureInstance(bool dontDestroyOnLoad)
         {
-            yield return coroutine;
-            Destroy(gameObject);
+            if (dontDestroyOnLoad)
+            {
+                if (persistentInstance) return persistentInstance;
+                GameObject go = new GameObject("Coroutine Helper - persistent");
+                persistentInstance = go.AddComponent<CoroutineHelper>();
+                DontDestroyOnLoad(go);
+                return persistentInstance;
+            }
+            else
+            {
+                if (instance) return instance;
+                GameObject go = new GameObject("Coroutine Helper");
+                instance = go.AddComponent<CoroutineHelper>();
+                return instance;
+            }
         }
     }
 }
